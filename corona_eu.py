2e6365d1549cdbce7161a22973dd3f59d0919972
Edyta Bredla <ebredla@gmail.com>
2020-07-01 20:27:04 +0200

    add file new version

--- corona_eu.py
+++ corona_eu.py
@@ -26,6 +26,8 @@ covid.get_data()
 #Extract countries names from geojson
 with open('europe.geojson') as data:
     geo = json.load(data)
+    
+    
 countries = []
 for item in geo['features']:
     countries.append(item['properties']['NAME'])
@@ -58,8 +60,6 @@ countries = ['Azerbaijan', 'Albania', 'Armenia', 'Bosnia and Herzegovina', 'Bulg
            'Portugal', 'Romania', 'Moldova', 'Slovenia', 'Spain', 'Sweden', 'Switzerland', 'Turkey', 'United Kingdom', 'Ukraine', 'San Marino', 'Serbia', 'Russia']
 
 ##no macedonia, montenegro
-
-
 
 population= []
 for land in countries:
@@ -107,12 +107,17 @@ korona = pd.read_csv(korona)
 
 
 #Folium map
-europe = os.path.join('/home/edyta/corona/','europe.geojson')
+#europe = os.path.join('/home/edyta/corona/','europe.geojson')
 m = folium.Map(location=[55, 10], zoom_start=4,  tiles='cartodbdark_matter')
 #geojson_layer = folium.GeoJson(europe,name='geojson', show=False).add_to(m)
 
+for item in geo['features']:
+    for rat in korona_ratio['Ratio']:
+        item['properties']['ratio'] = rat
+
+
 m.choropleth(
- geo_data=europe,
+ geo_data=geo,
  name='Corona',
  data=korona,
  columns=['Country', 'Cases'],
@@ -124,7 +129,7 @@ m.choropleth(
 
 
 m.choropleth(
- geo_data=europe,
+ geo_data=geo,
  name='Corona_ratio',
  data=korona_ratio,
  columns=['Country', 'Ratio'],
@@ -133,14 +138,31 @@ m.choropleth(
   threshold_scale=[0, 1,2, 620],
   show = False
 )
-
-
+style_function = lambda x: {'fillColor': '#ffffff', 
+                            'color':'#000000', 
+                            'fillOpacity': 0.1, 
+                            'weight': 0.1}
+highlight_function = lambda x: {'fillColor': '#000000', 
+                                'color':'#000000', 
+                                'fillOpacity': 0.50, 
+                                'weight': 0.1}
+NIL=folium.features.GeoJson(
+        geo,
+        style_function=style_function,
+        control=False,
+        highlight_function=highlight_function,
+        tooltip=folium.features.GeoJsonTooltip(fields=['NAME', 'ratio'],
+            aliases=['Name', 'R: '],
+            style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;"),
+            sticky=True
+        )
+    )
+m.add_child(NIL)
 
 #Add popup
-geo_json = folium.GeoJson(europe, popup=folium.GeoJsonPopup(fields=['NAME']), show =False)
-geo_json.add_to(m)
+#geo_json = folium.GeoJson(geo, popup=folium.GeoJsonPopup(fields=['NAME']), show =False)
+#geo_json.add_to(m)
 #Save to .html
 folium.LayerControl().add_to(m)
 
 m.save('Corona.html')
-
